467990a704be101ceb9db82cf01e68bc194281ac
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-06-28 10:55:32 +0700

    add redux-actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
-export function setTechnology(text) {
-    return {
-        type: 'SET_TECHNOLOGY',
-        payload: {
-            tech: text
-        }
-    }
-}
+import { createAction } from 'redux-actions';
+
+export const setTechnology = createAction('SET_TECHNOLOGY');
+
+// export function setTechnology(text) {
+//     return {
+//         type: 'SET_TECHNOLOGY',
+//         payload: {
+//             tech: text
+//         }
+//     }
+// }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,20 @@
-export default (state, action) => {
-    console.log(action.type);
-    switch (action.type) {
-        case 'SET_TECHNOLOGY':
-            return {
-                ...state,
-                tech: action.payload.tech
-            }
-        default:
-            return state;
-    }
-}
+import { handleAction } from 'redux-actions';
+
+export default handleAction(
+  'SET_TECHNOLOGY',
+  (state, action) => ({ ...state, tech: action.payload }),
+  {}
+);
+
+// export default (state, action) => {
+//     console.log(action.type);
+//     switch (action.type) {
+//         case 'SET_TECHNOLOGY':
+//             return {
+//                 ...state,
+//                 tech: action.payload.tech
+//             }
+//         default:
+//             return state;
+//     }
+// }
